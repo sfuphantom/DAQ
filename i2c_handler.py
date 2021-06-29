fb5aeef7f68850e7b3269f06684658ab01e8d5ca
ewong012 <58529979+ewong012@users.noreply.github.com>
2021-06-29 03:27:05 -0700

    Check connected i2c addresses before initializing ADC and IMU

--- i2c_handler.py
+++ i2c_handler.py
@@ -15,20 +15,38 @@ import RPi.GPIO as GPIO
 
 
 class i2c_handler(object):
+    ADC_ADDRESS = 0x48
+    IMU_ADDRESS = 0x6a
     IR_TEMPERATURE_SELECTOR_PINS = [17, 27, 22]  # GPIO pin numbering
     SHOCK_TRAVEL_SELECTOR_PINS = [23, 24]  # GPIO pin numbering
 
     def __init__(self, controller):
         self._controller = controller
         self._i2c = busio.I2C(board.SCL, board.SDA)
-        self._imu = LSM6DS33(self._i2c)
-        # self._imu.accelerometer_data_rate = Rate.RATE_12_5_HZ
-        # self._imu.accelerometer_range = AccelRange.RANGE_4G
-        # self._imu.gyro_data_rate = Rate.RATE_104_HZ
-        # self._imu.gyro_range = GyroRange.RANGE_250_DPS
-        self._adc = ADS.ADS1115(self._i2c)
-        self._adc_chan0 = AnalogIn(self._adc, ADS.P0)
-        self._adc_chan2 = AnalogIn(self._adc, ADS.P2)
+        self.adc_connected = False
+        self.imu_connected = False
+        while not self._i2c.try_lock():
+            pass
+        try:
+            i2c_addresses = self._i2c.scan()
+            if self.ADC_ADDRESS in i2c_addresses:
+                self.adc_connected = True
+                print("ADC is connected with address %s" % hex(self.ADC_ADDRESS))
+            else:
+                print("ADC is not connected")
+            if self.IMU_ADDRESS in i2c_addresses:
+                self.imu_connected = True
+                print("IMU is connected with address %s" % hex(self.IMU_ADDRESS))
+            else:
+                print("IMU is not connected")
+        finally:
+            self._i2c.unlock()
+
+        if self.adc_connected:
+            self._init_adc()
+        if self.imu_connected:
+            self._init_imu()
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(
             self.IR_TEMPERATURE_SELECTOR_PINS + self.SHOCK_TRAVEL_SELECTOR_PINS,
@@ -37,6 +55,18 @@ class i2c_handler(object):
         )
         self._i2c_thread = Thread(target=self.i2c_thread)
         self._i2c_thread.start()
+
+    def _init_adc(self):
+        self._adc = ADS.ADS1115(self._i2c)
+        self._adc_chan0 = AnalogIn(self._adc, ADS.P0)
+        self._adc_chan2 = AnalogIn(self._adc, ADS.P2)
+
+    def _init_imu(self):
+        self._imu = LSM6DS33(self._i2c)
+        # self._imu.accelerometer_data_rate = Rate.RATE_12_5_HZ
+        # self._imu.accelerometer_range = AccelRange.RANGE_4G
+        # self._imu.gyro_data_rate = Rate.RATE_104_HZ
+        # self._imu.gyro_range = GyroRange.RANGE_250_DPS
 
     def read_imu(self):
         dt = datetime.utcnow()
@@ -74,8 +104,11 @@ class i2c_handler(object):
 
     def i2c_thread(self):
         while True:
-            self.read_ir_temperature()
+            if self.adc_connected:
+                self.read_ir_temperature()
             for i in range(50):
-                self.read_imu()
-                self.read_shock_travel()
+                if self.imu_connected:
+                    self.read_imu()
+                if self.adc_connected:
+                    self.read_shock_travel()
                 sleep(0.02)
