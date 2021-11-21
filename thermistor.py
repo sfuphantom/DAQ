00e9071341e659462114a76cc93c438cc2c12891
Rustem Ismagilov <Rustem2001@gmail.com>
2021-11-21 11:12:57 -0800

    The temp. constant was converted in Kelvin

--- thermistor.py
+++ thermistor.py
@@ -3,11 +3,11 @@ def therm_get_temp(v_out):    # Takes voltage on thermistor as input and returns
     
     #Constants
     BETA = 3435 #property of the thermistor
-    T1 = 25
+    T1 = 298.15 # 25C in K
     R1 = 10000 # Known resistance of the thermistor at T1 temperature
     R_RES = 10000 #Resistance of the resistor in the circuit
     
     r_s = (v_out*R_RES)/(5-v_out) #Calculating a current resistance of the thermistor
-    t2 = (T1 * BETA)/(BETA - (math.log(R1/r_s) * T1)) # using obtained resistance to find a tempurature at the thermistor
+    t2 = (T1 * BETA)/(BETA - (math.log(R1/r_s) * T1)) - 273.15 # using obtained resistance to find a tempurature at the thermistor. Then convert it back to Celsius
     return t2
 
