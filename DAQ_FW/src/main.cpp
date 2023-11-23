8d0737a0959d1de998b164dcdb5fc285c0ed2272
lonamle <114131069+lonamle@users.noreply.github.com>
2023-11-23 12:18:14 -0800

    Add files via upload
    working code for 4 sensors

--- DAQ_FW/src/main.cpp
+++ DAQ_FW/src/main.cpp
@@ -1,26 +1,130 @@
 #include <Arduino.h>
-#include <ISensorDriver.h>
+#include <iostream>
+#include <wheelSpeed.h> 
 
-int sr = 4; // pin number
+// define sensor pins
+
+#define frontLeft_WSPIN 14 // wheel #1
+#define frontRight_WSPIN 16 // wheel #2
+#define backLeft_WSPIN 18 // wheel #3
+#define backRight_WSPIN 19 // wheel #4
+
+// hardware timer pointer
+hw_timer_t * timer = NULL;
+
+// define variables for counting
+
+int volatile WP1=0; 
+int volatile WP2=0; 
+int volatile WP3=0; 
+int volatile WP4=0; 
+
+int volatile timerCounter = 0;
+
+// interrupt service routines: count interrupts
+
+void IRAM_ATTR ISR1(){ WP1++;}
+
+void IRAM_ATTR ISR2(){ WP2++;}
+
+void IRAM_ATTR ISR3(){ WP3++;}
+
+void IRAM_ATTR ISR4(){ WP4++;}
+
+
+// interrupt reset functions: detach and reset interrupts after each period
+
+void interruptReset1(){
+  detachInterrupt(frontLeft_WSPIN);  
+  WP1= 0;
+  attachInterrupt(frontLeft_WSPIN, ISR1, FALLING);
+}
+
+void interruptReset2(){
+  detachInterrupt(frontRight_WSPIN);  
+  WP2= 0;
+  attachInterrupt(frontRight_WSPIN, ISR2, FALLING);
+}
+
+void interruptReset3(){
+  detachInterrupt(backLeft_WSPIN);
+  WP3= 0;
+  attachInterrupt(backLeft_WSPIN, ISR3, FALLING);
+}
+
+void interruptReset4(){
+  detachInterrupt(backRight_WSPIN);
+  WP4= 0;
+  attachInterrupt(backRight_WSPIN, ISR4, FALLING);
+}
+
+
+// timer interrupt service routine
+
+void IRAM_ATTR onTimer(){ timerCounter ++;}
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(sr, OUTPUT);
+
   Serial.begin(9600);
   Serial.println("Connected");
-};
+
+  pinMode(frontRight_WSPIN, INPUT);
+  pinMode(frontLeft_WSPIN, INPUT);
+  pinMode(backLeft_WSPIN, INPUT);
+  pinMode(backRight_WSPIN, INPUT);
+
+  // attach interrupt that adds to pulse count when pin goes from HIGH to LOW
+
+  attachInterrupt(frontLeft_WSPIN, ISR1, FALLING); 
+  attachInterrupt(frontRight_WSPIN, ISR2, FALLING); 
+  attachInterrupt(backLeft_WSPIN, ISR3, FALLING); 
+  attachInterrupt(backRight_WSPIN, ISR4, FALLING); 
+
+  // begin timer
+
+  timer = timerBegin(0, 80, true); 
+
+  timerAttachInterrupt(timer, &onTimer, true); // attach timer interrupt
+  timerAlarmWrite(timer, 1000000, true); // 1 second interrupt period
+  timerAlarmEnable(timer); // enable timer interrupt
+}
+
+
+// void wheelSpeedDisplay(int volatile wheelPulses, int wheelNumber){
+  
+//   // calculate RPM from wheel pulse count
+//   int RPM = wheelPulses/49*60; // subject to change based on tire 
+  
+//   // print results
+//   Serial.print("Wheel ");
+//   Serial.print(wheelNumber);
+//   Serial.print(" RPM = ");
+//   Serial.println(RPM);
+
+//   // print wheel pulse count for testing purposes
+//   Serial.print(wheelPulses);
+// }
+
 
 void loop() {
-  int data = analogRead(sr);
+   
+  // after 1 second, timer counter equals 1
+  // when timerCounter equals 1, compute wheel speed using wheel pulse values
 
-  float x=0.5; //temp sensor proportionality constant
+  if (timerCounter > 0){
+    timerCounter --;
 
-  // example implementation of functions
-  // data = brakePressure(data); 
+    wheelSpeedDisplay(WP1, 1);
+    interruptReset1();
 
-  //display data
-  Serial.print("Sensor reading=");
-  Serial.println(data);
+    wheelSpeedDisplay(WP2, 2);
+    interruptReset2();
 
-  delay(1000);
-  };
+    wheelSpeedDisplay(WP3, 3);
+    interruptReset3();
+
+    wheelSpeedDisplay(WP4, 4);
+    interruptReset4();
+    };
+}
---
+++ DAQ_FW/src/wheelSpeed.h
@@ -0,0 +1,9 @@
+#ifndef WHEELSPEED_H
+#define WHEELSPEED_H
+
+#include <Arduino.h>
+#include <iostream>
+
+void wheelSpeedDisplay(int volatile wheelPulses, int wheelNumber);
+
+#endif
