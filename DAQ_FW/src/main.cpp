d3378789480c6b025365588d2d1c9dd57525204b
seraermolenko <sera.ermolenko@gmail.com>
2024-07-13 13:46:32 -0700

    adding wheelspeed updates

--- DAQ_FW/src/main.cpp
+++ DAQ_FW/src/main.cpp
@@ -1,18 +1,30 @@
 #include "Logger.h"
 #include "IADCSensor.h"
+#include "wheelSpeed.h"
 
 // object declarations can't be done in setup()
 
 // params: sensorname, sensorID, adcAddress
 ChildExample SensorTest("TestSensor", 1, ADCAddress::U1);
 
+// cooloant pressure sensor object decleration
+CoolantPressureSensor CoolantPressure("CoolantPressureSensor", 2, ADCAddress::U1);
+
+// coolant tempature sensor object decleration
+CoolantTemperatureSensor CoolantTemperature("CoolantTemperature Sensor", 2, ADCAddress::U1);
+
 void setup()
 {
   // put your setup code here, to run once:
   Logger::Start();
   Logger::Notice("Setup");
+  WheelSpeedSetup();
 
-  SensorTest.Initialize();
+  //SensorTest.Initialize();
+
+  //CoolantPressure.Initialize();
+
+  //CoolantTemperature.Initialize();
 }
 
 void loop()
@@ -20,7 +32,12 @@ void loop()
   // put your main code here, to run repeatedly:
   Logger::Notice("Hello World");
 
-  SensorTest.GetData();
+  //SensorTest.GetData();
 
+  //CoolantPressure.GetData();
+
+  //CoolantTemperature.GetData();
+
+  WheelSpeedReset();
   delay(1000);
 }
