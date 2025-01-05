9e1ce8003b30d95f14e8cb04f8dc0dba854cc1a6
seraermolenko <sera.ermolenko@gmail.com>
2025-01-05 13:51:54 -0800

    Coolant fault signal logic

--- DAQ_FW/src/main.cpp
+++ DAQ_FW/src/main.cpp
@@ -1,6 +1,18 @@
 #include "Logger.h"
 #include "IADCSensor.h"
 #include "wheelSpeed.h"
+#include <Arduino.h>
+#include <driver/twai.h>
+
+MAX_TEMP = 4
+MIN_TEMP =  2
+MIN_PRESSURE = 1.3 
+MAX_PRESSURE = 1.7 
+
+#define FAULT_MSG_ID 0x100
+// CAN pins
+#define CAN_TX GPIO_NUM_21
+#define CAN_RX GPIO_NUM_22
 
 // object declarations can't be done in setup()
 
@@ -34,9 +46,28 @@ void loop()
 
   //SensorTest.GetData();
 
-  //CoolantPressure.GetData();
+  //float pressure = CoolantPressure.GetData();
 
-  //CoolantTemperature.GetData();
+  //float temp = CoolantTemperature.GetData();
+
+  if (pressure < MIN_PRESSURE || pressure > MAX_PRESSURE || temp < MIN_TEMP || temp > MAX_TEMP){
+    Logger::Error("Fault detected! Sending fault signal.");
+    faultDetected = true; 
+  }
+
+  twai_message_t faultMessage;
+  uint8_t faultSignal[] = {faultDetected ? 1 : 0}; // 1 = True
+  twai_message_t faultMessage;
+  faultMessage.identifier = FAULT_MSG_ID;  
+  faultMessage.extd = 0;                  // Standard ID
+  faultMessage.data_length_code = 1;      // 1 byte of data 
+  faultMessage.data[0] = faultSignal[0];  // Fault signal (1 or 0)
+
+  if (twai_transmit(&faultMessage, pdMS_TO_TICKS(1000)) == ESP_OK) {
+    Logger::Notice(faultDetected ? "Fault signal sent successfully" : "No fault signal sent successfully");
+  } else {
+    Logger::Error("Failed to send fault signal");
+  }
 
   WheelSpeedReset();
   delay(1000);
