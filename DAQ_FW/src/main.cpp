941759cab603d0fa104bd51af2936866386dc52e
seraermolenko <sera.ermolenko@gmail.com>
2025-02-03 20:59:58 -0800

    CANdriver

--- DAQ_FW/src/main.cpp
+++ DAQ_FW/src/main.cpp
@@ -4,11 +4,12 @@
 #include <Arduino.h>
 #include <driver/twai.h>
 #include "stdlib.h"
+#include "CanDriver.h"
 
-// MAX_TEMP = 4 write in define
-// MIN_TEMP =  2
-// MIN_PRESSURE = 1.3 
-// MAX_PRESSURE = 1.7 
+#define MAX_TEMP = 4
+#define MIN_TEMP =  2
+#define MIN_PRESSURE = 1.3 
+#define MAX_PRESSURE = 1.7 
 
 #define FAULT_MSG_ID 0x100
 #define WHEEL_MSG_ID 0x200
@@ -31,47 +32,41 @@ ChildExample SensorTest("TestSensor", 1, ADCAddress::U1);
 bool faultDetected = false;
 
 
-void sendFaultSignal(bool faultDetected) {
-    twai_message_t faultMessage;
-    faultMessage.identifier = FAULT_MSG_ID;
-    faultMessage.extd = 0;                 // Standard ID
-    faultMessage.data_length_code = 1;     // 1 byte of data
-    faultMessage.data[0] = faultDetected ? 1 : 0;
-
-    if (twai_transmit(&faultMessage, pdMS_TO_TICKS(1000)) == ESP_OK) {
-        Logger::Notice(faultDetected ? "Fault signal sent successfully" : "No fault detected, signal sent");
-    } else {
-        Logger::Error("Failed to send fault signal");
-    }
-}
-
 void sendWheelSpeeds(float frontLeft, float frontRight, float rearLeft, float rearRight) {
-    twai_message_t wheelMessage;
-    wheelMessage.identifier = WHEEL_MSG_ID; 
-    wheelMessage.extd = 0;                   // 11-bit ID
-    wheelMessage.data_length_code = 8;       // 8 bytes of data (2 bytes per wheel)
+    // twai_message_t wheelMessage;
+    // wheelMessage.identifier = WHEEL_MSG_ID; 
+    // wheelMessage.extd = 0;                   // 11-bit ID
+    // wheelMessage.data_length_code = 8;       // 8 bytes of data (2 bytes per wheel)
     
     // preserving 2 decimal places
     int16_t fl = (int16_t)(frontLeft * 100);
     int16_t fr = (int16_t)(frontRight * 100);
     int16_t rl = (int16_t)(rearLeft * 100);
     int16_t rr = (int16_t)(rearRight * 100);
-    
+    uint64_t wheel_data = (uint64_t)(wheelMessage.data)
     // extracting upper and lower bytes 
-    wheelMessage.data[0] = (uint8_t)(fl >> 8);    
-    wheelMessage.data[1] = (uint8_t)(fl & 0xFF);  
-    wheelMessage.data[2] = (uint8_t)(fr >> 8);   
-    wheelMessage.data[3] = (uint8_t)(fr & 0xFF);  
-    wheelMessage.data[4] = (uint8_t)(rl >> 8);    
-    wheelMessage.data[5] = (uint8_t)(rl & 0xFF);  
-    wheelMessage.data[6] = (uint8_t)(rr >> 8);    
-    wheelMessage.data[7] = (uint8_t)(rr & 0xFF);  
+    // wheelMessage.data[0] = (uint8_t)(fl >> 8);    
+    // wheelMessage.data[1] = (uint8_t)(fl & 0xFF);  
+    // wheelMessage.data[2] = (uint8_t)(fr >> 8);   
+    // wheelMessage.data[3] = (uint8_t)(fr & 0xFF);  
+    // wheelMessage.data[4] = (uint8_t)(rl >> 8);    
+    // wheelMessage.data[5] = (uint8_t)(rl & 0xFF);  
+    // wheelMessage.data[6] = (uint8_t)(rr >> 8);    
+    // wheelMessage.data[7] = (uint8_t)(rr & 0xFF);  
     
-    if (twai_transmit(&wheelMessage, pdMS_TO_TICKS(1000)) == ESP_OK) {
-        Logger::Notice("Wheel speeds sent successfully");
-    } else {
-        Logger::Error("Failed to send wheel speeds");
-    }
+    uint64_t wheel_data = 0;
+    wheel_data |= ((uint64_t)(uint16_t)fl << 48);
+    wheel_data |= ((uint64_t)(uint16_t)fr << 32);
+    wheel_data |= ((uint64_t)(uint16_t)rl << 16);
+    wheel_data |= ((uint64_t)(uint16_t)rr);
+
+    CanDriver::sendCanData(NULL, 1, 2, wheel_data, false);
+
+    // if (twai_transmit(&wheelMessage, pdMS_TO_TICKS(1000)) == ESP_OK) {
+    //     Logger::Notice("Wheel speeds sent successfully");
+    // } else {
+    //     Logger::Error("Failed to send wheel speeds");
+    // }
 }
 
 void setup()
@@ -79,11 +74,13 @@ void setup()
   Logger::Start();
   Logger::Notice("Setup");
   //WheelSpeedSetup();
-  // CanDriver::CanInit();
+  CanDriver::CanInnit();
 
   SensorTest.Initialize();
   //CoolantPressure.Initialize();
   //CoolantTemperature.Initialize();
+
+  CanDriver::sendCanData(NULL, 1, 2, 0, false);
 }
 
 // Main
@@ -95,14 +92,11 @@ void loop()
   //float pressure = CoolantPressure.GetData();
   //float temp = CoolantTemperature.GetData();
 
-  // if (pressure < MIN_PRESSURE || pressure > MAX_PRESSURE || temp < MIN_TEMP || temp > MAX_TEMP){
-  //   Logger::Error("Fault detected! Sending fault signal.");
-  //   faultDetected = true; 
-  // } else {
-  //       faultDetected = false;
-  //   }
+  if (pressure < MIN_PRESSURE || pressure > MAX_PRESSURE || temp < MIN_TEMP || temp > MAX_TEMP){
+    Logger::Error("Fault detected! Sending fault signal.");
+    CanDriver::sendCanData(NULL, 1, 2, 1, false); 
+  } 
 
-  // sendFaultSignal(faultDetected);
   // sendWheelSpeeds(fl, fr, rl, rr);
   
   //WheelSpeedReset();
