8bef0fe9b294a08f03450a12e0e3050024389d6e
seraermolenko <sera.ermolenko@gmail.com>
2025-11-04 17:20:31 -0800

    loopback can

--- DAQ_FW/lib/Can/can.cpp
+++ DAQ_FW/lib/Can/can.cpp
@@ -4,7 +4,7 @@
 void CAN_Init()
 {
     can_general_config_t general_config = {
-        .mode = CAN_MODE_NO_ACK,
+        .mode = CAN_MODE_NO_ACK, //CAN_MODE_LOOPBACK,
         .tx_io = GPIO_NUM_5,
         .rx_io = GPIO_NUM_4,
         .clkout_io = CAN_IO_UNUSED,
@@ -26,6 +26,26 @@ void CAN_Init()
     }
 
     Logger::Notice("CAN Initialized (TWAI)");
+
+    Logger::Notice("Testing CAN Loopback...");
+
+    can_message_t msg = {};
+    msg.identifier = 0x321;
+    msg.data_length_code = 2;
+    msg.data[0] = 0xAB;
+    msg.data[1] = 0xCD;
+
+    can_transmit(&msg, pdMS_TO_TICKS(1000));
+
+    can_message_t rx;
+    if (can_receive(&rx, pdMS_TO_TICKS(1000)) == ESP_OK)
+    {
+        Logger::Notice("LOOPBACK OK — ID: 0x%X, data: [%X %X]", rx.identifier, rx.data[0], rx.data[1]);
+    }
+    else
+    {
+        Logger::Error("LOOPBACK FAILED");
+    }
 }
 
 void CAN_SendInt16(uint16_t id, int16_t value)
