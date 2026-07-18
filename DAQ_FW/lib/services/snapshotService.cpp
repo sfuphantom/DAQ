01a30480d051e3221e266004ed00a439be0e9abc
seraermolenko <sera.ermolenko@gmail.com>
2026-07-17 23:55:11 -0700

    Add per-subsystem timestamps to sensor snapshot

--- DAQ_FW/lib/services/snapshotService.cpp
+++ DAQ_FW/lib/services/snapshotService.cpp
@@ -1,46 +1,42 @@
-#include "SnapshotService.h"
+#include "snapshotService.h"
 
 static SensorSnapshot gSnapshot = {};
 static portMUX_TYPE gSnapshotMux = portMUX_INITIALIZER_UNLOCKED;
 
-void SnapshotService_UpdateCritical(float temp1, float temp2, float flow1Lpm, float flow2Lpm, bool faultActive, uint32_t timestampMs)
-{
+void snapshotServiceUpdateCritical(float temp1, float temp2, float flow1Lpm, float flow2Lpm, bool faultActive, uint32_t timestampMs) {
     portENTER_CRITICAL(&gSnapshotMux);
     gSnapshot.temp1 = temp1;
     gSnapshot.temp2 = temp2;
     gSnapshot.flow1Lpm = flow1Lpm;
     gSnapshot.flow2Lpm = flow2Lpm;
     gSnapshot.faultActive = faultActive;
-    gSnapshot.timestampMs = timestampMs;
+    gSnapshot.criticalTimestampMs = timestampMs;
     portEXIT_CRITICAL(&gSnapshotMux);
 }
 
-void SnapshotService_UpdateWheelSpeed(float speedFL, float speedFR, float speedRL, float speedRR, float speedKmh, uint32_t timestampMs)
-{
+void snapshotServiceUpdateWheelSpeed(float speedFL, float speedFR, float speedRL, float speedRR, float speedKmh, uint32_t timestampMs) {
     portENTER_CRITICAL(&gSnapshotMux);
     gSnapshot.speedFL = speedFL;
     gSnapshot.speedFR = speedFR;
     gSnapshot.speedRL = speedRL;
     gSnapshot.speedRR = speedRR;
     gSnapshot.speedKmh = speedKmh;
-    gSnapshot.timestampMs = timestampMs;
+    gSnapshot.wheelSpeedTimestampMs = timestampMs;
     portEXIT_CRITICAL(&gSnapshotMux);
 }
 
-void SnapshotService_UpdateChassis(float susp1, float susp2, float susp3, float susp4, float steeringAngleDeg, uint32_t timestampMs)
-{
+void snapshotServiceUpdateChassis(float susp1, float susp2, float susp3, float susp4, float steeringAngleDeg, uint32_t timestampMs) {
     portENTER_CRITICAL(&gSnapshotMux);
     gSnapshot.susp1 = susp1;
     gSnapshot.susp2 = susp2;
     gSnapshot.susp3 = susp3;
     gSnapshot.susp4 = susp4;
     gSnapshot.steeringAngleDeg = steeringAngleDeg;
-    gSnapshot.timestampMs = timestampMs;
+    gSnapshot.chassisTimestampMs = timestampMs;
     portEXIT_CRITICAL(&gSnapshotMux);
 }
 
-SensorSnapshot SnapshotService_Read()
-{
+SensorSnapshot snapshotServiceRead() {
     SensorSnapshot snapshot;
     portENTER_CRITICAL(&gSnapshotMux);
     snapshot = gSnapshot;
--- DAQ_FW/lib/services/snapshotService.h
+++ DAQ_FW/lib/services/snapshotService.h
@@ -4,8 +4,7 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/portmacro.h>
 
-struct SensorSnapshot
-{
+struct SensorSnapshot {
     float temp1;
     float temp2;
     float flow1Lpm;
@@ -21,10 +20,12 @@ struct SensorSnapshot
     float speedRR;
     float speedKmh;
     bool faultActive;
-    uint32_t timestampMs;
+    uint32_t criticalTimestampMs;
+    uint32_t chassisTimestampMs;
+    uint32_t wheelSpeedTimestampMs;
 };
 
-SensorSnapshot SnapshotService_Read();
-void SnapshotService_UpdateCritical(float temp1, float temp2, float flow1Lpm, float flow2Lpm, bool faultActive, uint32_t timestampMs);
-void SnapshotService_UpdateWheelSpeed(float speedFL, float speedFR, float speedRL, float speedRR, float speedKmh, uint32_t timestampMs);
-void SnapshotService_UpdateChassis(float susp1, float susp2, float susp3, float susp4, float steeringAngleDeg, uint32_t timestampMs);
+SensorSnapshot snapshotServiceRead();
+void snapshotServiceUpdateCritical(float temp1, float temp2, float flow1Lpm, float flow2Lpm, bool faultActive, uint32_t timestampMs);
+void snapshotServiceUpdateWheelSpeed(float speedFL, float speedFR, float speedRL, float speedRR, float speedKmh, uint32_t timestampMs);
+void snapshotServiceUpdateChassis(float susp1, float susp2, float susp3, float susp4, float steeringAngleDeg, uint32_t timestampMs);
